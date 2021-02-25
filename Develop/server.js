4a2893923855d840a05a3cb6d851eb49f13f4096
Jennifer Ross <jjannross@gmail.com>
2021-02-25 15:53:32 -0500

    added routes, fixed server.js, added uuid

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+// const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+const displayRoutes = require("./routes/displayRoute.js");
+const apiRoutes = require("./routes/apiRoute.js");
+app.use(displayRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+  console.log("listening on PORT " + PORT);
+});
